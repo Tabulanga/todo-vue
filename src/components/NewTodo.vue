04cf7ea69a7ae8ded8c41a106023d3f27ab526a0
Tabulanga <bokor@tabulan.ga>
2019-04-18 22:42:17 +0700

    worked

---
+++ src/components/NewTodo.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  .new
+    h1
+      | Новая задача
+    | Заголовок:
+    br
+    input(v-model='todoItem.title')
+    br
+    | Теги (разделенные запятой):
+    br
+    input(v-model='tagStr')
+    br
+    | Описание задачи:
+    br
+    textarea(v-model='todoItem.description' placeholder="введите подробности")
+    br
+    | Дата выполнения:
+    br
+    input(v-model='selectedDate' type='date')
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'newTodo',
+  data () {
+    return {
+      tagStr: '',
+      selectedDate: '2019-04-18', 
+      todoItem: {
+        title: '',
+        deadline: selectedDate,
+        description: '',
+        tags: [],
+
+        status: '',
+        expired: false,
+        edited: true
+      }
+    }
+  },
+  computed: { ...mapState([ 'todoList' ]) },
+  methods: {
+    setDeadline (date) {
+      this.todoItem.deadline = new Date(date);
+    },
+    
+  },
+  mounted () {
+    console.log(this.todoList)
+  }
+}
+</script>
---
+++ src/views/AddTodo.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .add
+    new-todo
+</template>
+
+<script>
+import NewTodo from '@/components/NewTodo'
+
+export default {
+  name: 'home',
+  components: {
+    NewTodo
+  }
+}
+</script>
